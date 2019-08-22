d680caf4a04e5a132ee5acbbc28b6ba646301bb3
Mauro <mcvio78@gmail.com>
2019-08-23 00:32:33 +0200

    now /about-us shows component with name: about and /about redirect to component with name: about

--- src/router.js
+++ src/router.js
@@ -12,13 +12,17 @@ export default new Router({
       component: Home
     },
     {
-      path: "/about",
+      path: "/about-us",
       name: "about",
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () =>
         import(/* webpackChunkName: "about" */ "./views/About.vue")
+    },
+    {
+      path: "/about",
+      redirect: { name: "about" }
     }
   ]
 });
