2428e9caeb2ad6e80089954f3774da48ba711a76
Mauro <mcvio78@gmail.com>
2019-08-23 12:42:29 +0200

    create router-link to event-show and pass id params to it

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,5 +1,14 @@
 <template>
-  <h1>Events Listing</h1>
+  <div>
+    <h1>Events Listing</h1>
+    <router-link :to="{ name: 'event-show', params: { id: 1 } }"
+      >Show Event #1</router-link
+    >
+    <br />
+    <router-link :to="{ name: 'event-show', params: { id: 2 } }"
+      >Show Event #2</router-link
+    >
+  </div>
 </template>
 
 <script>
