cf8fcc4a56e83355f12f5acb03262b45e7ccc09d
Mauro <mcvio78@gmail.com>
2019-08-23 12:30:16 +0200

    render username as props

--- src/views/User.vue
+++ src/views/User.vue
@@ -1,10 +1,11 @@
 <template>
-  <h1>This is {{ $route.params.username }}'s page</h1>
+  <h1>This is {{ username }}'s page</h1>
 </template>
 
 <script>
 export default {
-  name: "User"
+  name: "User",
+  props: ["username"]
 };
 </script>
 
