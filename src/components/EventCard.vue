4478fe2ce99dd6a8138802d64df2ad2a93c5d81b
Mauro <mcvio78@gmail.com>
2019-08-23 14:18:14 +0200

    create classes'style and wrap eventCard with router-link to event-show passing id = 1 as params

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -1,9 +1,11 @@
 <template>
-  <div class="event-card -shadow">
-		<span class="eyebrow">@{{ event.time}} at {{ event.date}}</span>
-    <h4 class="title">{{ event.title }}</h4>
-		<span>{{ event.attendees.length}} attending</span>
-  </div>
+	<router-link class="event-link" :to="{ name: 'event-show', params: { id: 1 } }">
+		<div class="event-card -shadow">
+			<span class="eyebrow">@{{ event.time }} at {{ event.date }}</span>
+			<h4 class="title">{{ event.title }}</h4>
+			<span>{{ event.attendees.length }} attending</span>
+		</div>
+	</router-link>
 </template>
 
 <script>
@@ -26,7 +28,23 @@ export default {
 </script>
 
 <style scoped>
-h4 {
-  color: green;
+.event-card {
+  padding: 20px;
+  margin-bottom: 24px;
+  transition: all 0.2s linear;
+  cursor: pointer;
+}
+.event-card:hover {
+  transform: scale(1.01);
+  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2), 0 1px 15px 0 rgba(0, 0, 0, 0.19);
+}
+.event-card > .title {
+  margin: 0;
+}
+
+.event-link {
+  color: black;
+  text-decoration: none;
+  font-weight: 100;
 }
 </style>
