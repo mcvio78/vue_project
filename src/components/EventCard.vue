4c35d0444587e025057012c93a2478e8f3a9e9fa
Mauro <mcvio78@gmail.com>
2019-08-23 14:10:45 +0200

    create an event object in data field

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -8,7 +8,16 @@
 export default {
   data() {
     return {
-      title: 'Beach Cleanup'
+      event: {
+        id: 1,
+        title: 'Beach Cleanup',
+        date: 'Tues Aug 19, 2018',
+        time: '6:00',
+        attendees: [
+          { id: 'abc123', name: 'Adam Jahr' },
+          { id: 'def456', name: 'Gregg Pollack' }
+        ]
+      }
     };
   }
 };
