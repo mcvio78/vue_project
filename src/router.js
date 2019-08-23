2cca55fb5f6cce0b9f3da1fc5d02a7e87dad31d3
Mauro <mcvio78@gmail.com>
2019-08-23 14:47:25 +0200

    automatic global registration, components starting with "Base" and ending in .vue or .js

--- src/router.js
+++ src/router.js
@@ -1,12 +1,46 @@
 import Vue from 'vue';
 import Router from 'vue-router';
+import upperFirst from 'lodash/upperFirst';
+import camelCase from 'lodash/camelCase';
+//
 import EventList from './views/EventList';
 import EventShow from './views/EventShow';
 import EventCreate from './views/EventCreate';
 import Error_404 from './views/Error_404';
-import BaseIcon from './components/BaseIcon.vue';
 
-Vue.component('BaseIcon', BaseIcon);
+const requireComponent = require.context(
+  // The relative path of the components folder
+  './components',
+  // Whether or not to look in subfolders
+  false,
+  // The regular expression used to match base component filenames
+  /Base[A-Z]\w+\.(vue|js)$/
+);
+
+requireComponent.keys().forEach(fileName => {
+  // Get component config
+  const componentConfig = requireComponent(fileName);
+  console.log('componentConfig: ', componentConfig);
+  // Get PascalCase name of component
+  const componentName = upperFirst(
+    camelCase(
+      // Gets the file name regardless of folder depth
+      fileName
+        .split('/')
+        .pop()
+        .replace(/\.\w+$/, '')
+    )
+  );
+
+  // Register component globally
+  Vue.component(
+    componentName,
+    // Look for the component options on `.default`, which will
+    // exist if the component was exported with `export default`,
+    // otherwise fall back to module's root.
+    componentConfig.default || componentConfig
+  );
+});
 
 Vue.use(Router);
 
