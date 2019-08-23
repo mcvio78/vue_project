bcd2d326353576d444266dc6be0ce1c55b81ee80
Mauro <mcvio78@gmail.com>
2019-08-23 12:41:10 +0200

    create error_404 module

---
+++ src/views/Error_404.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="back">
+    <h1>404 Page Not Found.</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Error_404"
+};
+</script>
+
+<style scoped>
+h1 {
+  color: red;
+}
+</style>
