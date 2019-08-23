59fa1472bac6a40f70d8082da646be32a451129e
Mauro <mcvio78@gmail.com>
2019-08-23 14:19:41 +0200

    remove router-link to event-show

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -4,9 +4,6 @@
 
     <EventCard />
 
-    <router-link :to="{ name: 'event-show', params: { id: 1 } }"
-      >Show Event #1</router-link
-    >
     <br />
     <router-link :to="{ name: 'event-show', params: { id: 2 } }"
       >Show Event #2</router-link
