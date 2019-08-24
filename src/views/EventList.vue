d8b17337d6a1851706233227272819aa3657b939
Mauro <mcvio78@gmail.com>
2019-08-24 19:13:05 +0200

    on created dispatch fetchEvents with perPage and page, get current page with $route.query.page,

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,7 +1,17 @@
 <template>
 	<div>
-		<h1>Events Listing</h1>
 		<EventCard v-for="event in events" :key="event.id" :event="event" />
+		<template v-if="page != 1">
+			<router-link
+				:to="{ name: 'event-list', query: { page: page - 1 } }"
+				rel="prev"
+				>Prev Page</router-link
+			>
+			|
+		</template>
+		<router-link :to="{ name: 'event-list', query: { page: page + 1 } }"
+			>Next Page</router-link
+		>
 	</div>
 </template>
 
@@ -14,8 +24,17 @@ export default {
 		EventCard
 	},
 	created() {
-		this.$store.dispatch('fetchEvents');
+		this.$store.dispatch('fetchEvents', {
+			perPage: 3, // <-- How many items to display per page
+			page: this.page // <-- What page we're on
+		});
 	},
-	computed: mapState(['events'])
+	computed: {
+		page() {
+			// What page we're currently on
+			return parseInt(this.$route.query.page) || 1;
+		},
+		...mapState(['events'])
+	}
 };
 </script>
