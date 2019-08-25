403351da668ccc3246d2444636c19ce0b24598f4
Mauro <mcvio78@gmail.com>
2019-08-25 12:36:07 +0200

    create event folder, and pass getters, actions, mutations and state(except categories, event, eventsTotal, events) from store import and use it on store

---
+++ src/store/modules/event.js
@@ -0,0 +1,73 @@
+import EventService from '@/services/EventService.js';
+
+export const state = {
+	categories: [
+		'sustainability',
+		'nature',
+		'animal welfare',
+		'housing',
+		'education',
+		'food',
+		'community'
+	],
+	events: [],
+	eventsTotal: 0,
+	event: {}
+};
+
+export const mutations = {
+	SET_EVENTS(state, events) {
+		state.events = events;
+	},
+	ADD_EVENT(state, event) {
+		state.events.push(event);
+	},
+	SET_EVENTS_TOTAL(state, eventsTotal) {
+		state.eventsTotal = eventsTotal;
+	},
+	SET_EVENT(state, event) {
+		state.event = event;
+	}
+};
+
+export const actions = {
+	fetchEvents({ commit }, { perPage, page }) {
+		EventService.getEvents(perPage, page)
+			.then(response => {
+				commit('SET_EVENTS_TOTAL', parseInt(response.headers['x-total-count']));
+				commit('SET_EVENTS', response.data);
+			})
+			.catch(error => {
+				console.log('There was an error:', error.response);
+			});
+	},
+	createEvent({ commit }, event) {
+		return EventService.postEvent(event).then(() => {
+			commit('ADD_EVENT', event.data);
+		});
+	},
+	fetchEvent({ commit, getters }, id) {
+		// Send in the getters
+		let event = getters.getEventById(id); // See if we already have this event
+
+		if (event) {
+			// If we do, set the event
+			commit('SET_EVENT', event);
+		} else {
+			// If not, get it with the API.
+			EventService.getEvent(id)
+				.then(response => {
+					commit('SET_EVENT', response.data);
+				})
+				.catch(error => {
+					console.log('There was an error:', error.response);
+				});
+		}
+	}
+};
+
+export const getters = {
+	getEventById: state => id => {
+		return state.events.find(event => event.id === id);
+	}
+};
--- src/store/store.js
+++ src/store/store.js
@@ -1,13 +1,14 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-import EventService from '@/services/EventService.js';
-import * as user from '@/store/modules/user.js'
+import * as user from '@/store/modules/user.js';
+import * as event from '@/store/modules/event.js'
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
 	modules: {
-		user
+		user,
+		event
 	},
 	state: {
 		categories: [
@@ -22,59 +23,5 @@ export default new Vuex.Store({
 		events: [],
 		eventsTotal: 0,
 		event: {}
-	},
-	mutations: {
-		SET_EVENTS(state, events) {
-			state.events = events;
-		},
-		ADD_EVENT(state, event) {
-			state.events.push(event);
-		},
-		SET_EVENTS_TOTAL(state, eventsTotal) {
-			state.eventsTotal = eventsTotal;
-		},
-		SET_EVENT(state, event) {
-			state.event = event
-		}
-	},
-	actions: {
-		fetchEvents({ commit }, { perPage, page }) {
-			EventService.getEvents(perPage, page)
-				.then(response => {
-					commit(
-						'SET_EVENTS_TOTAL',
-						parseInt(response.headers['x-total-count'])
-					);
-					commit('SET_EVENTS', response.data);
-				})
-				.catch(error => {
-					console.log('There was an error:', error.response);
-				});
-		},
-		createEvent({ commit }, event) {
-			return EventService.postEvent(event).then(() => {
-				commit('ADD_EVENT', event.data);
-			});
-		},
-		fetchEvent({ commit, getters }, id) {  // Send in the getters
-			let event = getters.getEventById(id) // See if we already have this event
-
-			if (event) { // If we do, set the event
-				commit('SET_EVENT', event)
-			} else {  // If not, get it with the API.
-				EventService.getEvent(id)
-				.then(response => {
-					commit('SET_EVENT', response.data)
-				})
-				.catch(error => {
-					console.log('There was an error:', error.response)
-				})
-			}
-		}
-	},
-	getters: {
-		getEventById: state => id => {
-			return state.events.find(event => event.id === id);
-		}
 	}
 });
