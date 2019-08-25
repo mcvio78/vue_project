b12639cf041d7cdcd417538d864b44cf48523be9
Mauro <mcvio78@gmail.com>
2019-08-25 12:15:58 +0200

    create modules folder, create user.js file inside and put user data in it, import in store user module and use it as a module, on EventList and EventCreate now we must to add another user. on template,

---
+++ src/store/modules/user.js
@@ -0,0 +1,5 @@
+export const state = {
+	user: {
+		id: 'abc123',
+		name: 'Mauro' },
+}
--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,6 +1,6 @@
 <template>
 	<div>
-		<h1>Events for {{ user.name }}</h1>
+		<h1>Events for {{ user.user.name }}</h1>
 		<h1>Events Listing</h1>
 		<EventCard v-for="event in events" :key="event.id" :event="event" />
 		<template v-if="page != 1">
